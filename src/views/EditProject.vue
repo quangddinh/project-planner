fdad9d4d0cf252ce4903aeb14d02726970a4f373
Dinh Quang <quangddinhnt@gmail.com>
2021-05-05 19:12:29 +0700

    project planner

---
+++ src/views/EditProject.vue
@@ -0,0 +1,55 @@
+<template>
+    <form @submit.prevent="handleUpdate">
+        <label>Title</label>
+        <input type="text" v-model="title" required />
+        <label>Details</label>
+        <textarea v-model="details" required></textarea>
+        <button>Update Project</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: ["id"],
+    // set props : true ở router index.js
+    data() {
+        return {
+            title: "",
+            // ----> v model
+            details: "",
+            // ----> v model
+
+            // truyen props de hien thi data
+            // => id
+            uri: "http://localhost:3000/projects/" + this.id,
+        };
+    },
+    methods: {
+        handleUpdate() {
+            fetch(this.uri, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    title: this.title,
+                    details: this.details,
+                }),
+            })
+                .then(() => {
+                    this.$router.push("/");
+                })
+                .catch((err) => console.log(err));
+        },
+    },
+    mounted() {
+        fetch(this.uri)
+            .then((res) => res.json())
+            .then((data) => {
+                this.title = data.title;
+                this.details = data.details;
+            })
+            .catch((err) => console.log(err));
+    },
+};
+</script>
+
+<style></style>
